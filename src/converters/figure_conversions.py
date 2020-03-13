8fcd6982be2e564e83b00963a0ad4e8ab6b542b3
Matej Kubinec <32638572+matejkubinec@users.noreply.github.com>
2020-03-13 22:48:46 +0100

    #5 - Plot images in higher resolution
    Closes #5

--- src/converters/figure_conversions.py
+++ src/converters/figure_conversions.py
@@ -1,10 +1,11 @@
 from io import StringIO, BytesIO
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from drawing.drawing_constants import dpi
 
 
 def fig_to_png(fig):
     image_data = BytesIO()
-    fig.savefig(image_data, format="png")
+    fig.savefig(image_data, format="png", dpi=dpi)
     image_data.seek(0)
     del fig
     return image_data.getvalue()
@@ -12,7 +13,7 @@ def fig_to_png(fig):
 
 def fig_to_svg(fig):
     image_data = StringIO()
-    fig.savefig(image_data, format="svg")
+    fig.savefig(image_data, format="svg", dpi=dpi)
     image_data.seek(0)
     del fig
     return image_data.read()
