db8d3b0c891e1d65415c4d5a8dd7783ba8678f8b
Matej Kubinec <32638572+matejkubinec@users.noreply.github.com>
2020-03-13 21:46:13 +0100

    #4 - Added application insights
    Closes #4

--- src/app.py
+++ src/app.py
@@ -1,15 +1,19 @@
+from applicationinsights.flask.ext import AppInsights
 from flask import Flask, request, Response, send_from_directory, jsonify
 from flask_swagger import swagger
 from controllers.single_layer_controller import single_layer
 from controllers.multi_layer_controller import multi_layer
 from controllers.common_charts_controller import common_charts
 import json
+import os
 
 app = Flask(__name__)
+app.config["APPINSIGHTS_INSTRUMENTATIONKEY"] = os.environ["APPINSIGHTS_INSTRUMENTATIONKEY"]
 app.register_blueprint(single_layer)
 app.register_blueprint(multi_layer)
 app.register_blueprint(common_charts)
 
+appinsights = AppInsights(app)
 
 @app.route("/spec")
 def spec():
